1b42b2ecee8eb42360cdf4ea05b32575342ef891
Santiagoo <santiagominotta2913@hotmail.com>
2026-01-10 23:18:45 -0800

    Update cart.js to add placeholders for future functions

---
+++ scripts/cart.js
@@ -0,0 +1,22 @@
+// FOR DAY 3
+
+console.log('Cart module');
+
+// Placeholder for cart functions
+window.cart = {
+    items: [],
+    total: 0,
+    
+    addItem: function(product) {
+        console.log(`addItem function to implement in Day 3`);
+    },
+    
+    removeItem: function(productId) {
+        console.log(`removeItem function to implement in Day 3`);
+    },
+    
+    calculateTotal: function() {
+        console.log(`calculateTotal function to implement in Day 3`);
+        return 0;
+    }
+};
